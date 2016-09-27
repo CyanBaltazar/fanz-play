42dfe127bbe9dcf524c19c9d4d3c5b71e9a5a0f1
Christian Ryan Baltazar <technodeveloper15@gmail.com>
2016-09-27 16:06:07 +0800

    Add files via upload

---
+++ Fanz Play/js/register-account.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+
+    var navListItems = $('div.setup ul li a'),
+            allWells = $('.tab-content'),
+            nextBtn = $('.reg-next-button');
+
+    allWells.hide();
+
+    navListItems.click(function (e) {
+        e.preventDefault();
+        var $target = $($(this).attr('href')),
+                $item = $(this);
+
+        if (!$item.hasClass('disabled')) {
+            navListItems.removeClass('btn-primary').addClass('btn-default');
+            $item.addClass('btn-primary');
+            allWells.hide();
+            $target.show();
+            $target.find('input:eq(0)').focus();
+        }
+    });
+
+    nextBtn.click(function(){
+        var curStep = $(this).closest(".setup-content"),
+            curStepBtn = curStep.attr("id"),
+            nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+            curInputs = curStep.find("input[type='text'],input[type='url']"),
+            isValid = true;
+
+
+        if (isValid)
+            nextStepWizard.removeAttr('disabled').trigger('click');
+    });
+
+    $(navListItems).trigger('click');
+});
