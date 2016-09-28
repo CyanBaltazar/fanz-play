7cdee78e68d6ce4c8c8c73dd65a2647a88fec296
Ryan Baltazar <ryan.baltazar@outresource.com.au>
2016-09-28 13:07:38 +0800

    Fixed footer.

--- Fanz Play/js/register-account.js
+++ Fanz Play/js/register-account.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $(".reg-next-button").click(function(){
+    $(".reg-button").click(function(){
         $("#form-1").hide();
         $("#form-2").show();
     });
