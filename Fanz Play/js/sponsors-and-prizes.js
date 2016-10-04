96b37e11e1fc2ca9339073b470c234da42722974
Ryan Baltazar <ryan.baltazar@outresource.com.au>
2016-10-04 08:59:08 +0800

    Created Sponsors and Prizes Page.

---
+++ Fanz Play/js/sponsors-and-prizes.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $(".close-1").click(function() {
+    $(".first-record").hide();
+  });
+  $(".close-2").click(function() {
+    $(".second-record").hide();
+  });
+  $(".close-3").click(function() {
+    $(".third-record").hide();
+  });
+  $(".close-4").click(function() {
+    $(".fourth-record").hide();
+  });
+});
