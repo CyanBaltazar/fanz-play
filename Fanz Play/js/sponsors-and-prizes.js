f811d384404bb17c4ad4a2c59cc0d61dd966b150
Ryan Baltazar <ryan.baltazar@outresource.com.au>
2016-10-06 11:41:11 +0800

    Edit CSS for the Sponsor and Prizes page.

--- Fanz Play/js/sponsors-and-prizes.js
+++ Fanz Play/js/sponsors-and-prizes.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-  $("table tbody tr:nth-child(1) td:nth-child(6)").click(function() {
+  $("table tbody tr:nth-child(1) td:nth-child(6) a span").click(function() {
     $("table tbody tr:nth-child(1)").hide();
   });
-  $("table tbody tr:nth-child(2) td:nth-child(6)").click(function() {
+  $("table tbody tr:nth-child(2) td:nth-child(6) a span").click(function() {
     $("table tbody tr:nth-child(2)").hide();
   });
-  $("table tbody tr:nth-child(3) td:nth-child(6)").click(function() {
+  $("table tbody tr:nth-child(3) td:nth-child(6) a span").click(function() {
     $("table tbody tr:nth-child(3)").hide();
   });
-  $("table tbody tr:nth-child(4) td:nth-child(6)").click(function() {
+  $("table tbody tr:nth-child(4) td:nth-child(6) a span").click(function() {
     $("table tbody tr:nth-child(4)").hide();
   });
 });
