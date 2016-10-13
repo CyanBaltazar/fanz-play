e6e7b0f8acc5aca0b6d3838ab485df5c87427050
Ryan Baltazar <ryan.baltazar@outresource.com.au>
2016-10-13 14:26:17 +0800

    Place the searchbox css to main.css, changed the delete prompt modal, responsiveness of login page.

--- Fanz Play/js/sponsors-and-prizes.js
+++ Fanz Play/js/sponsors-and-prizes.js
@@ -1,14 +1,18 @@
-$(document).ready(function() {
-  $("table tbody tr:nth-child(1) td:nth-child(6) a span").click(function() {
-    $("table tbody tr:nth-child(1)").hide();
-  });
-  $("table tbody tr:nth-child(2) td:nth-child(6) a span").click(function() {
-    $("table tbody tr:nth-child(2)").hide();
-  });
-  $("table tbody tr:nth-child(3) td:nth-child(6) a span").click(function() {
-    $("table tbody tr:nth-child(3)").hide();
-  });
-  $("table tbody tr:nth-child(4) td:nth-child(6) a span").click(function() {
-    $("table tbody tr:nth-child(4)").hide();
-  });
-});
+// $(document).ready(function() {
+//   $("table tbody tr:nth-child(1) td:nth-child(6) a span").click(function() {
+//     $("table tbody tr:nth-child(1)").hide();
+//   });
+//   $("table tbody tr:nth-child(2) td:nth-child(6) a span").click(function() {
+//     $("table tbody tr:nth-child(2)").hide();
+//   });
+//   $("table tbody tr:nth-child(3) td:nth-child(6) a span").click(function() {
+//     $("table tbody tr:nth-child(3)").hide();
+//   });
+//   $("table tbody tr:nth-child(4) td:nth-child(6) a span").click(function() {
+//     $("table tbody tr:nth-child(4)").hide();
+//   });
+// });
+
+// function deleteRow(x) {
+//     alert("Row index is: " + x.rowIndex);
+// }
