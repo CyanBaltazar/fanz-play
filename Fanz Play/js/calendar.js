b835a4522a5fc3ef530ba0a4d206c2d2cba7c1eb
Ryan Baltazar <ryan.baltazar@outresource.com.au>
2016-10-19 08:28:02 +0800

    Calendar page, fix nav menu issues of all pages and made them uniform, Create trivia set page.

--- Fanz Play/js/calendar.js
+++ Fanz Play/js/calendar.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
+  // bind html view with the calendar js
   $( '#calendar' ).calendario();
 
+  // calculate month and year
   $(function() {
 
         var cal = $( '#calendar' ).calendario( {
@@ -47,4 +49,102 @@ $(document).ready(function() {
         */
 
       });
+
+
+      $(function () {
+        $(' #custom-prev ').on( 'click' , function () {
+          $(this).css('background-color', 'transparent');
+          $(this).css('color', '#ffffff');
+        });
+
+        $(' #custom-next ').on( 'click' , function () {
+          $(this).css('background-color', 'transparent');
+          $(this).css('color', '#ffffff');
+        });
+
+        $(' #custom-current ').on( 'click' , function () {
+          $(this).css('background-color', 'transparent');
+          $(this).css('color', '#00BCD4');
+        });
+      });
+
+      
+
+
+      // Previous and next date functionality
+      $(function() {
+
+        var transEndEventNames = {
+            'WebkitTransition' : 'webkitTransitionEnd',
+            'MozTransition' : 'transitionend',
+            'OTransition' : 'oTransitionEnd',
+            'msTransition' : 'MSTransitionEnd',
+            'transition' : 'transitionend'
+          },
+          transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+          $wrapper = $( '#custom-inner' ),
+          $calendar = $( '#calendar' ),
+          cal = $calendar.calendario( {
+            onDayClick : function( $el, $contentEl, dateProperties ) {
+
+              if( $contentEl.length > 0 ) {
+                showEvents( $contentEl, dateProperties );
+              }
+
+            },
+            caldata : codropsEvents,
+            displayWeekAbbr : true
+          } ),
+          $month = $( '#custom-month' ).html( cal.getMonthName() ),
+          $year = $( '#custom-year' ).html( cal.getYear() );
+
+        $( '#custom-next' ).on( 'click', function() {
+          cal.gotoNextMonth( updateMonthYear );
+        } );
+        $( '#custom-prev' ).on( 'click', function() {
+          cal.gotoPreviousMonth( updateMonthYear );
+        } );
+
+        function updateMonthYear() {
+          $month.html( cal.getMonthName() );
+          $year.html( cal.getYear() );
+        }
+
+        // just an example..
+        function showEvents( $contentEl, dateProperties ) {
+
+          hideEvents();
+
+          var $events = $( '<div id="custom-content-reveal" class="custom-content-reveal"><h4>Events for ' + dateProperties.monthname + ' ' + dateProperties.day + ', ' + dateProperties.year + '</h4></div>' ),
+            $close = $( '<span class="custom-content-close"></span>' ).on( 'click', hideEvents );
+
+          $events.append( $contentEl.html() , $close ).insertAfter( $wrapper );
+
+          setTimeout( function() {
+            $events.css( 'top', '0%' );
+          }, 25 );
+
+        }
+        function hideEvents() {
+
+          var $events = $( '#custom-content-reveal' );
+          if( $events.length > 0 ) {
+
+            $events.css( 'top', '100%' );
+            Modernizr.csstransitions ? $events.on( transEndEventName, function() { $( this ).remove(); } ) : $events.remove();
+
+          }
+
+        }
+      });
+
+      // Add class to custom-month and custom-year
+      $(function () {
+        $('#custom-month').attr('class', 'custom-month');
+        $('#custom-year').attr('class', 'custom-year');
+      });
+
+
+
+
 });
--- Fanz Play/js/main.js
+++ Fanz Play/js/main.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
   // Change active status (Navigation)
-  $('ul li a').click(function(){
+  $('ul li a').click(function () {
     $('li a').removeClass("active");
     $(this).addClass("active");
-});
+  });
+  // Change the icon from right icon to down icon in mobile nav menu
+  $(".glyphicon-chevron-right").click(function () {
+    $(this).toggleClass("glyphicon-chevron-down");
+  });
+
 });
